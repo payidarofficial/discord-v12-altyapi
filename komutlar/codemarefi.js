2efb54569a0db224b08199878a95f312e5e99c9b
Pâyidar <79402324+payidarofficial@users.noreply.github.com>
2021-03-13 16:00:34 +0300

    Add files via upload

---
+++ komutlar/codemarefi.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js')
+
+    exports.run = async(client, message, args) => {
+        // Kodlarınızı Bu Bölüğe Giriniz. İyi Kullanımlar
+    }
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: ['Pâyidar'],
+    permLevel: 0
+}
+
+exports.name = {
+    name: 'pâyidar'
+}
